f06f3849213534e68c529b5188be3f021d9827bf
Erin Kinnen <erin.kinnen@gmail.com>
2017-06-11 17:26:42 -0500

    initial commit

---
+++ src/helloworld_java/HelloWorld_Java.java
@@ -0,0 +1,18 @@
+
+package helloworld_java;
+
+/**
+ *
+ * @author erinkinnen
+ */
+public class HelloWorld_Java {
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Hello World");
+    }
+    
+}
